bceca2728a8f8672351388f949b674c0d700fe74
withkenik <withkenik@gmail.com>
2025-08-09 06:27:33 +0900

    docker first commit

---
+++ src/test/java/org/doit/ik/Devops02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doit.ik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
